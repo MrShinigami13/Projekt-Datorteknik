01388957d89674eddfec26dbbefb2c25bef25455
Nandeyanen123 <emickols@kth.se>
2017-02-27 17:19:42 +0100

    Lagt till lite variabler
    -

--- PONG-spel/mipslabwork.c
+++ PONG-spel/mipslabwork.c
@@ -39,9 +39,32 @@ int prime = 1234567;
 int timeoutcounter = 0;
 int bindis = 0;
 int mytime = 0x5957;
-int paddle_y = 0;
+int paddle1_x = 0;
+int paddle1_y = 12;
+int paddle2_x = 126;
+int paddle2_y = 12;
+int ball_x = 62;
+int ball_y = 14;
+int ball_speed = 0;
+int player1score = 0;
+int player2score = 0;
 
 char textstring[] = "text, more text, and even more text!";
+
+unsigned int rand (void)
+{
+   static unsigned int z1 = 12345, z2 = 12345, z3 = 12345, z4 = 12345;
+   unsigned int b;
+   b  = ((z1 << 6) ^ z1) >> 13;
+   z1 = ((z1 & 4294967294U) << 18) ^ b;
+   b  = ((z2 << 2) ^ z2) >> 27; 
+   z2 = ((z2 & 4294967288U) << 2) ^ b;
+   b  = ((z3 << 13) ^ z3) >> 21;
+   z3 = ((z3 & 4294967280U) << 7) ^ b;
+   b  = ((z4 << 3) ^ z4) >> 12;
+   z4 = ((z4 & 4294967168U) << 13) ^ b;
+   return (z1 ^ z2 ^ z3 ^ z4);
+}
 
 /* Interrupt Service Routine */
 void user_isr( void )
