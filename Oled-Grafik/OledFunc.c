96598768c871406415b14fa02d419e295f590567
Nandeyanen123 <emickols@kth.se>
2017-02-28 18:08:10 +0100

    ch-ch-ch-ch-changes

--- Oled-Grafik/OledFunc.c
+++ Oled-Grafik/OledFunc.c
@@ -7,7 +7,6 @@
 extern int		xcoOledCur;
 extern int		ycoOledCur;
 extern char *	pbOledCur;
-extern char		rgbOledBmp[];
 
 extern int		bnOledCur;
 extern char		clrOledCur;
@@ -40,7 +39,7 @@ void OledMoveTo(int xco, int yco)
 	/* Compute the display access parameters corresponding to
 	** the specified position.
 	*/
-	pbOledCur = &rgbOledBmp[((yco/8) * ccolOledMax) + xco];
+	pbOledCur = &textbuffer[((yco/8) * ccolOledMax) + xco];
 	bnOledCur = yco & 7;
 
 }
@@ -129,7 +128,7 @@ OledPutBmp(int dxco, int dyco, char * pbBits)
 	bnAlign = ycoTop & 0x07;
 	mskUpper = (1 << bnAlign) - 1;
 	mskLower = ~mskUpper;
-	pbDspLeft = &rgbOledBmp[((ycoTop/8) * ccolOledMax) + xcoLeft];
+	pbDspLeft = &textbuffer[((ycoTop/8) * ccolOledMax) + xcoLeft];
 	pbBmpLeft = pbBits;
 	fTop = 1;
 
@@ -225,7 +224,7 @@ OledMoveUp()
 		/* If we have gone off of the top of the display,
 		** go back down.
 		*/
-		if (pbOledCur < rgbOledBmp) {
+		if (pbOledCur < textbuffer) {
 			pbOledCur += ccolOledMax;
 		}
 	}
@@ -265,7 +264,7 @@ OledMoveDown()
 		/* If we have gone off the end of the display memory
 		** go back up a page.
 		*/
-		if (pbOledCur >= rgbOledBmp+cbOledDispMax) {
+		if (pbOledCur >= textbuffer+cbOledDispMax) {
 			pbOledCur -= ccolOledMax;
 		}
 	}
@@ -294,7 +293,7 @@ OledMoveLeft()
 
 	/* Are we at the left edge of the display already
 	*/
-	if (((pbOledCur - rgbOledBmp) & (ccolOledMax-1)) == 0) {
+	if (((pbOledCur - textbuffer) & (ccolOledMax-1)) == 0) {
 		return;
 	}
 
@@ -327,7 +326,7 @@ OledMoveRight()
 
 	/* Are we at the right edge of the display already
 	*/
-	if (((pbOledCur-rgbOledBmp) & (ccolOledMax-1)) == (ccolOledMax-1)) {
+	if (((pbOledCur-textbuffer) & (ccolOledMax-1)) == (ccolOledMax-1)) {
 		return;
 	}
 
