26dd1bd0c99224c031a43d895a7a3d7cf73af6ee
oskar <omol@kth.se>
2017-04-02 20:34:58 +0200

    ny mapp

--- PONG-spel-optimerat.X/nbproject/Makefile-impl.mk
+++ PONG-spel-optimerat.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=PONG-spel-new.X
+PROJECTNAME=PONG-spel-optimerat.X
 
 # Active Configuration
 DEFAULTCONF=default
